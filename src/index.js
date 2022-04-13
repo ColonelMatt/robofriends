ec980779743d5f0e4b40bca7ae6b566ca9c89bf2
ColonelMatt <103227875+ColonelMatt@users.noreply.github.com>
2022-04-12 21:36:31 -0400

    Robot list + loop

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import Hello from './Hello.js';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+//import Card from './Card';
+import CardList from './CardList';
+import { robots } from './robots';
 
 
 //added greeting property <p>{this.props.greeting}</p> in Hello.js (main app file)
 ReactDOM.render(
   <React.StrictMode>
-    <Hello greeting={'Hello React Ninja'} />
+    <CardList robots={robots} />
   </React.StrictMode>,
   document.getElementById('root')
 );
