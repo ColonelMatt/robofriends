247cf1b22cf6d8f24d606df9defe5b2fdd39c37d
ColonelMatt <matthesecure@gmail.com>
2022-04-14 20:14:33 -0400

    306 building udemy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
+import SearchBox from './SearchBox';
 
-
-const App = () => {
-    return(
-        <CardList robots={robots} />
-    )
+const state = {
+    
 }
+
+
+class App extends Component{
+    render()
+        {
+            constructor()
+            {
+                super()
+                this.state = {
+                    robots: robots,
+                    searchfield: ''
+                }
+            }
+            return(
+                <div className='tc'>
+                    <h1>RoboFriends</h1>
+                    <SearchBox />
+                    <CardList robots={robots} />
+                </div>
+                )
+        }
+
+}
+
+    
+
 
 export default App
